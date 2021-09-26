120e62c2b7a9e7fc3894f65b20d8d4492218842d
TarikMetinKaya <48484623+TarikMetinKaya@users.noreply.github.com>
2021-09-26 13:06:53 +0300

    upt 1

--- Strings/basicStringFunctions.py
+++ Strings/basicStringFunctions.py
@@ -33,16 +33,56 @@ def setStringLength(string,length,align:"left"):
                     rigth_text += " "
             return left_text + string + rigth_text
 
-        if not align in ("left", "mid", "right"):
+        if align not in ("left", "mid", "right"):
             print("Align must be \"left\", \"mid\" or \"right\" !!")
             return
 
 
 
     elif real_str_len>length:
-        print("kesilecek")
-    #TODO: string ifadenin içierisinde dizi gibi gezin mid için de hangisi ortaysa o indexten başla falan bir sağ bir sol saf matematik
 
+        if align=="left":
+            return string[0:length]
+        elif align=="right":
+            return string[-(length):]
+        elif align=="mid":
+            mid_index_of_text=int(real_str_len/2)
+            if length%2==0:
+                left_length=int(length/2)
+                rigth_length=int(length/2)
+            elif length%2==1:
+                left_length=int(length/2)+1
+                rigth_length=int(length/2)
 
+            var_x=int(mid_index_of_text-left_length+1)
+            var_y=int(mid_index_of_text+rigth_length+1)
 
-print(setStringLength("naber",7,align="mid"))
+            return string[var_x:var_y]
+        if not align in ("left", "mid", "right"):
+            print("Align must be \"left\", \"mid\" or \"right\" !!")
+            return
+
+def addCharOnString(string,char,amount,type:"pre-post"):
+    """
+
+    :param string: Text is which you want to add smth pre, post or both
+    :param char: Must be just one character
+    :param amount: int
+    :param type: pre, post, pre-post
+    :return:
+    """
+    if type not in ("pre","post","pre-post"):
+        print("Type must be \"pre\", \"post\" or \"pre-post\" !!")
+        return
+    if not len(char)==1:
+        print("Lenght of char must be 1")
+    text_which_add=""
+    for i in range(0,amount):
+        text_which_add += f"{char}"
+    if type == "pre":
+        return text_which_add+string
+    elif type == "post":
+        return string+text_which_add
+    elif type == "pre-post":
+        return text_which_add+string+text_which_add
+
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import Strings.basicStringFunctions as basicStringF
 
+txt=basicStringF.setStringLength("123456789",3,"mid")
+print(txt)
+
+txt=basicStringF.addCharOnString("Tarik Metin Kaya","#",5,"pre-post")
+print(txt)
