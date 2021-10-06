ca058e1f54be641de6653b50f024c2dddb04b682
TarikMetinKaya <48484623+TarikMetinKaya@users.noreply.github.com>
2021-10-06 21:26:28 +0300

    tinyUrlApi

---
+++ APIs/tinyUrlApi.py
@@ -0,0 +1,17 @@
+import requests
+from ignoreConfig import tinyUrlApiToken
+
+def urlShortener(URL):
+    try:
+        url_main = f'https://api.tinyurl.com/create?api_token={tinyUrlApiToken}'
+        myobj = {
+          "url": URL,
+          "domain": "tiny.one",
+        }
+
+        x = requests.post(url_main, data = myobj).json()
+
+        return x['data']['tiny_url']
+    except Exception as e:
+        print(e)
+        return URL
