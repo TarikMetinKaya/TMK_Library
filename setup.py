fee5c2883aa427f8123b5f91f00abefb00b7473e
TarikMetinKaya <48484623+TarikMetinKaya@users.noreply.github.com>
2021-10-06 22:00:16 +0300

    setup3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='tmk_tiny_library',
+    name='tmktinylibrary',
     version='0.0.1',
     author='Tarik Metin Kaya',
     author_email='tarikmetink@gmail.com',
